16ee19862315135d76fca34d21a68226569a6321
cjl <13528041139@163.com>
2024-09-09 14:32:25 +0800

    登录校验完成

---
+++ src/apis/user.ts
@@ -0,0 +1,13 @@
+import http from '@/utils/http'
+
+export const loginAPI = ({ account, password }) => {
+    return http({
+        url: '/login',
+        method: 'post',
+        data: {
+            account,
+            password
+        }
+    })
+
+}
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import { ElMessage } from "element-plus";
+import 'element-plus/theme-chalk/el-message.css'
 
 const http = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -10,6 +12,10 @@ http.interceptors.request.use(config => {
 }, e => Promise.reject(e))
 
 http.interceptors.response.use(res => res.data, e => {
+    ElMessage({
+        type: 'warning',
+        message: e.response.data.message
+    })
     return Promise.reject(e)
 })
 export default http
