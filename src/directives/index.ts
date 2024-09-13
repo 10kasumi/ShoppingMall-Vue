08ef063646d14a64c4d8ab4e23c78568eb322bec
cjl <13528041139@163.com>
2024-09-13 15:54:04 +0800

    更新一些错误

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -1,9 +1,9 @@
 import { useIntersectionObserver } from '@vueuse/core'
 export const lazyPlugin = {
-    install(app) {
+    install(app: any) {
         app.directive('img-lazy', {
             //el：指令绑定的元素(img), binding：binding.value 指令等于后后面绑定的表达式的值(图片url)
-            mounted(el, binding) {
+            mounted(el: HTMLImageElement, binding: any) {
                 const { stop } = useIntersectionObserver(
                     el,
                     ([{ isIntersecting }]) => {
