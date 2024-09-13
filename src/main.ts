a3e32a5e6fb78cf20661ffcf1069d47ba2968e30
cjl <13528041139@163.com>
2024-09-13 16:49:33 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,24 @@ import router from './router'
 //引入初始化样式文件
 import '@/styles/common.scss'
 //引入懒加载插件并注册
-import { lazyPlugin } from '@/directives'
+// import { lazyPlugin } from '@/directives'
 import { componentPlugin } from '@/components'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import VueLazyload from 'vue-lazyload'
+import LazyPic from '@/assets/LazyPic.webp'
 
 const app = createApp(App)
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 app.use(pinia)
 app.use(router)
-app.use(lazyPlugin)
+// app.use(lazyPlugin)
+app.use(VueLazyload, {
+    error: LazyPic,
+    loading: LazyPic,
+    attempt: 3,
+    observer: true,
+})
 app.use(componentPlugin)
 app.mount('#app')
 
